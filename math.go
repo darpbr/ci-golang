a69f4b9d8d998c8f695c09b1a64ebe302392e710
darpbr <dipeixoto@gmail.com>
2022-09-15 16:04:38 -0300

    fix: corrigindo quebra da alteracao anterior

--- math.go
+++ math.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(soma(10, 20))
+	fmt.Println(Soma(10, 20))
 }
 
-func soma(a int, b int) int {
+func Soma(a int, b int) int {
 	return a + b
 }
