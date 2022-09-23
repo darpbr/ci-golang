985d629f6ccbafe6784d2958b0fc45c460f53ea3
darpbr <dipeixoto@gmail.com>
2022-09-22 22:40:40 -0300

    ci: check quality sonar cloud

--- math.go
+++ math.go
@@ -10,14 +10,14 @@ func soma(a int, b int) int {
 	return a + b
 }
 
-func sub(a int, b int) int {
-	return a - b
-}
+// func sub(a int, b int) int {
+// 	return a - b
+// }
 
-func mult(a int, b int) int {
-	return a * b
-}
+// func mult(a int, b int) int {
+// 	return a * b
+// }
 
-func div(a int, b int) int {
-	return a / b
-}
+// func div(a int, b int) int {
+// 	return a / b
+// }
